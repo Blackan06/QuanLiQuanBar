2c55a9b9e5971575956d87d386f75dd36b90e0c5
trungduc1122 <100517525+trungduc1122@users.noreply.github.com>
2022-03-02 18:39:01 +0700

    add BussinessLayer
    add Account

---
+++ Account.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class Account
+{
+	public string UserName { get; set; }
+	public string password { get; set; }
+	public string Name { get; set; }
+	public int admin { get; set; }
+}
