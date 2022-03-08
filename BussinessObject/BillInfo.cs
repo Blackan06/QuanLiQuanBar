0d094cab254e2de4f365794f69031ce063296e53
trungduc1122 <100517525+trungduc1122@users.noreply.github.com>
2022-03-08 07:11:00 +0700

    update to bussiness object
    add bill info
    add drink
    add drink category
    add food
    add food category
    add table bar

---
+++ BussinessObject/BillInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessObject
+{
+    public class BillInfo
+    {
+        public int id { get; set; }
+        public int idBill { get; set; }
+        public int idFood { get; set; }
+        public int idDrink { get; set; }
+        public int soluong { get; set; }
+
+    }
+}
---
+++ BussinessObject/drinkCategory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessObject
+{
+    public class drinkCategory
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        
+    }
+}
